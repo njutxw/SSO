580ef3fa18f0b473c8e72fc74494e234766ae4ae
njutxw <1352375760@qq.com>
2020-06-28 22:38:47 +0800

    2020/6/28 commit

--- sso-login/src/main/java/com/sso/login/controller/LoginController.java
+++ sso-login/src/main/java/com/sso/login/controller/LoginController.java
@@ -1,12 +1,45 @@
 package com.sso.login.controller;
 
-
+import com.sso.login.pojo.User;
+import com.sso.login.utils.LoginCacheUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//@Controller
-//@ResponseBody
-@RestController
+import javax.servlet.http.HttpSession;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    private static Set<User> dbUsers;
+    static{
+        dbUsers = new HashSet<>();
+        dbUsers.add(new User(0,"zhangsan","123456"));
+        dbUsers.add(new User(1,"lisi","1234567"));
+        dbUsers.add(new User(2,"wangwu","123"));
+    }
+
+    @PostMapping()
+    public String doLogin(User user,HttpSession session){
+        String target = (String) session.getAttribute("target");
+
+        Optional<User> first = dbUsers.stream().filter(users -> users.getUsername().equals(user.getUsername()) &&
+                users.getPassword().equals(user.getPassword())).findFirst();
+        if(first.isPresent()){
+            String token = UUID.randomUUID().toString();
+            LoginCacheUtil.loginUser.put(token,first.get());
+        }else{
+            session.setAttribute("msg","用户名或密码错误");
+            return "login";
+        }
+        //重定向到target地址
+        return "redirect"+target;
+    }
+
 }
--- sso-login/src/main/java/com/sso/login/controller/ViewController.java
+++ sso-login/src/main/java/com/sso/login/controller/ViewController.java
@@ -4,6 +4,10 @@ package com.sso.login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
 
 //页面跳转逻辑
 @Controller
@@ -12,7 +16,12 @@ public class ViewController {
 
     //跳转到登录页面
     @GetMapping("/login")
-    public String toLogin(){
+    public String toLogin(@RequestParam(required = false,defaultValue = "") String target,
+                          HttpSession session){
+        if(!StringUtils.isEmpty(target)){
+            target = "http://www.codeshop.com:9010";
+        }
+        session.setAttribute("target",target);
         return "login";
     }
 }
